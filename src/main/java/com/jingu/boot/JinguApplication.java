c8995182556376aec4e062aa5d2428bae1228cc5
gdw0908 <gdw0908@gmail.com>
2022-05-03 22:48:41 +0900

    초기생성

---
+++ src/main/java/com/jingu/boot/JinguApplication.java
@@ -0,0 +1,13 @@
+package com.jingu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jingu/boot/JinguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jingu.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
